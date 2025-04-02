2c8c9c52f73cb39733131e83682d2c4d70a860a7
Antonino-Sofia <antoninosofia420@gmail.com>
2025-04-02 15:33:17 +0200

    js-fizzbuzz

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 console.log("Antonino");
+
+// Cosa Voglio fare
+//scrivere un ciclo for che parte da 1 e arriva a 100
+//se è divisibile per 3 stampo fizz, se per 5 stampo buzz, se è divisibile per 3 e 5 stampo fizzbuzz
+
+for (let index = 1; index <= 100; index++) {}
