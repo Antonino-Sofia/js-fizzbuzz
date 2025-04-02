9a047891114909004a83524592cfc86081a59996
Antonino-Sofia <antoninosofia420@gmail.com>
2025-04-02 17:38:01 +0200

    js-fizzbuzz

--- js/main.js
+++ js/main.js
@@ -9,3 +9,25 @@ for (let index = 1; index <= 100; index++) {}
 let result = "";
 if (index % 3) result += "Fizz";
 if (index % 5) result += "Buzz";
+console.log(result || index);
+//
+for (let index = 1; index <= 100; index++) {}
+//
+const isDivByFive = index % 5 === 0;
+const isDivByThree = index % 3 === 0;
+
+if (isDivByFive && isDivByThree) {
+}
+console.log("FizzBuzz");{
+}
+else if (isDivByFive){
+}
+console.log ('Fizz');
+
+else if (isDivByThree) {
+    console.log('Buzz');
+  } else {
+    console.log(index);
+  }
+
+
